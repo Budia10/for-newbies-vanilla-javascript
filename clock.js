67d2552ea73ea5e6911c04daf7177416eac7dafe
Coding Cat <39578293+Budia10@users.noreply.github.com>
2019-04-11 18:18:01 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,20 +1,16 @@
 const clockContainer = document.querySelector(".js-clock"),
-    colckTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
     const date = new Date();
-    const minuate = date.getMinutes();
     const hours = date.getHours();
+    const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    colckTitle.innerText =
-        `${hours < 10 ? `0${hours}` : hours
-        }:${minuate < 10 ? `0${minuate}` : minuate
-        }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function init() {
     getTime();
-    setInterval(getTime, 50);
 }
 
 init();
--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentClass = title.className
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
-    }
+    // const currentClass = title.className
+    // const hasClass = title.classList.contains(CLICKED_CLASS)
+
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // } else {
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
