d01ee64736a1d6ac0489811f300b312e08b29f31
Coding Cat <39578293+Budia10@users.noreply.github.com>
2019-04-15 22:41:34 +0900

    Add files via upload

--- gretting.js
+++ gretting.js
@@ -1,45 +1,27 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+    greeting = document.querySelector(".js-greetings")
 
-const USER_LS = "currentUsers",
-    SHOWING_CN = "showing"; // css-display: block
+// const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
-}
-
-function handleSubmit(event){ // 누군가 서브밋 했을 때
-    event.preventDefault();
-    const currentValue = input.value;
-    console.log(currentValue);
-    paintGreeting(currentValue); // 페인트 그리팅
-    saveName(currentValue);
-}
-
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
-}
-
-function paintGreeting(text) {
+function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN); //greeting 변수에 showing 클래스 추가
-    greeting.innerText = `hello ${text}`
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) {
-        askForName();
+    const currentUser = localStorage.getItem("currentUser");
+    if(currentUser === null){
+        // She is not
     } else {
-        paintGreeting(currentUser);
-        // 유저가 있을 경우
+        paintGreeting(currentUser); // She is
     }
-};
+}
 
 function init() {
-    loadName();  //로드네임 함수 실행
+    loadName();
 }
 
 init();
